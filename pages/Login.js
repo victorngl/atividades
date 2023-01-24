d6da31eaabcf6e25ad808abb00f0e8f05f1328ea
Joao Victor Costa <victor_ngl2@hotmail.com>
2023-01-24 13:13:42 -0300

    inscricoes nao liberadas

--- pages/Login.js
+++ pages/Login.js
@@ -102,7 +102,7 @@ export default function Login({ children }) {
           </div>
 
         }
-
+        {/*
         <form onSubmit={handleSubmit}>
           <>
             <div className='text-lg text-center mb-6 font-bold text-lg font-medium'>Informe suas credenciais</div>
@@ -120,9 +120,12 @@ export default function Login({ children }) {
 
           <div className='flex justify-center'>
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Enviar</button>
+            
           </div>
-
+          
         </form>
+        */}
+        <p className='text-lg font-semibold'>As incrições ainda não iniciaram.</p>
       </div>
     </div >
 
--- pages/api/get_login.js
+++ pages/api/get_login.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
       aluno: false, // Return all fields
     },
   })
-  
+
   return res.status(200).json({ login: login })
   
 }
